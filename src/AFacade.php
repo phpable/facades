6e26bbb278a445bcb2afb1e27d67bad67a1f87e1
hacpaka <hacpaka@eggbe.com>
2018-06-06 02:26:13 +0300

    providing of arguments added

--- src/AFacade.php
+++ src/AFacade.php
@@ -22,13 +22,13 @@ abstract class AFacade {
 	 * @return object
 	 * @throws \Exception
 	 */
-	private final static function prepareRecipient(): object {
+	private final static function prepare(): object {
 		if (!isset(self::$Cache[static::class])){
 			if (!class_exists(static::$Recipient)){
 				throw new \Exception('Undefined recepient class!');
 			}
 
-			self::$Cache[static::class] = new static::$Recipient();
+			self::$Cache[static::class] = new static::$Recipient(...static::provide());
 		}
 
 		return self::$Cache[static::class];
@@ -41,6 +41,14 @@ abstract class AFacade {
 	 * @throws \Exception
 	 */
 	public final static function __callStatic(string $name, array $Args) {
-		return self::prepareRecipient()->{$name}(...$Args);
+		return self::prepare()->{$name}(...$Args);
 	}
+
+	/**
+	 * Provides arguments for a recipient object
+	 * creation if necessary.
+	 *
+	 * @return array
+	 */
+	protected abstract static function provide(): array;
 }
