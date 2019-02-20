b6c90724312795902194c2a9cb43840ac382274c
hacpaka <hacpaka@eggbe.com>
2019-02-20 11:44:20 +0300

    refactoring

--- src/AFacade.php
+++ src/AFacade.php
@@ -2,13 +2,10 @@
 namespace Able\Facades;
 
 use \Able\Facades\Structures\SInit;
-
-use \Able\Prototypes\TUnclonable;
-use \Able\Prototypes\TUncreatable;
+use \Able\Statics\TStatic;
 
 abstract class AFacade {
-	use TUncreatable;
-	use TUnclonable;
+	use TStatic;
 
 	/**
 	 * @var string
@@ -18,7 +15,7 @@ abstract class AFacade {
 	/**
 	 * @var bool
 	 */
-	protected static $keepSingleInstance  = true;
+	protected static $keepSingle  = true;
 
 	/**
 	 * @var array
@@ -27,14 +24,14 @@ abstract class AFacade {
 
 	/**
 	 * @return object
-	 * @throws \Throwable
+	 * @throws \Exception
 	 */
 	private final static function prepare(): object {
-		if (!static::$keepSingleInstance || !isset(self::$Cache[static::class])){
+		if (!static::$keepSingle || !isset(self::$Cache[static::class])){
 
 			/**
-			 * Each inheriting class has to be obligatorily linked
-			 * to a real recipient class.
+			 * Each inheriting class has to be linked
+			 * to a represented recipient class.
 			 */
 			if (!class_exists(static::$Recipient)){
 				throw new \Exception('Undefined recepient class!');
@@ -44,14 +41,14 @@ abstract class AFacade {
 				$Initializer = static::initialize();
 
 				/**
-				 * The argument list required by the recipient class constructor
-				 * must be provided via the related field of the initializing structure.
+				 * The argument list is required by the recipient class constructor
+				 * and must be provided via the related field of the initializing structure.
 				 */
 				self::$Cache[static::class] = new static::$Recipient(...$Initializer->args);
 
 				/**
-				 * The second field of structure could contain an initialize method.
-				 * It has to be called right after the creation of the recipient instance.
+				 * The second field of structure could contain an initialize method
+				 * to be called right after the creation of the recipient instance.
 				 */
 				if (!is_null($Initializer->handler)){
 					call_user_func($Initializer->handler, self::$Cache[static::class]);
@@ -66,18 +63,10 @@ abstract class AFacade {
 	}
 
 	/**
-	 * @return object
-	 * @throws \Throwable
-	 */
-	public final static function getRecipientInstance(): object {
-		return self::prepare();
-	}
-
-	/**
 	 * @param string $name
 	 * @param array $Args
 	 * @return mixed
-	 * @throws \Throwable
+	 * @throws \Exception
 	 */
 	public final static function __callStatic(string $name, array $Args) {
 		return self::prepare()->{$name}(...$Args);
